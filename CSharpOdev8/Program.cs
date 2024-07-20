1f34436db83f859f9b1a1c751f7afffa4df50b09
Halit TİRYAKİ <halittiryaki61@outlook.com>
2024-07-20 16:43:36 +0300

    Add project files.

---
+++ CSharpOdev8/Program.cs
@@ -0,0 +1,21 @@
+﻿//burada tanımlamalar ve kullanıcıdan değer alma var
+Console.WriteLine("Lütfen bir değer giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+//While
+while (count < limit)
+{
+    Console.WriteLine($"{count}Ben bir Patika'lıyım");
+    count++;
+}
+
+//Do While
+count = 0;
+do
+{
+    Console.WriteLine($"{count}Ben bir Patika'lıyım");
+    count++;
+} while (count < limit);
+
+//While ile do while arasında ki en önemli fark do while döngümüzde önce işlemlerin yapılması daha sonrasında koşul kontrol edilmesi yapılır. While da ise önce koşul kontrol edilir daha sonra işlemler yapılır. Diğer bir fark ise do while da en az 1 kez kodlama çalışır fakat while da koşula bağlı olarak hiç çalışmayabilir.
---
+++ CSharpOdev8/README.md
@@ -0,0 +1,2 @@
+#while ile do while örneði
+Bu uygulamamýzda bir örneði iki farklý döngü þekli ile çalýþtýrdýk ve bu iki döngü þekli arasýnda ki farklarý anlattýk.
